b93e732e4851d48ad77ba65aae1ea335dd73e5a3
Fvazquez <foapgenerico@365.stucom.com>
2025-06-17 18:31:08 +0200

    stucom

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const path = require('node:path');
 const express = require('express');
 
 const app = express();
-process.loadEnvFile();
-const port = process.env.PORT || 8888;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+process.loadEnvFile();
+const PORT = process.env.PORT || 8888;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`Example app listening on http://localhost:${PORT}`));
