d861fd5c74ac30c114846c72dc39f0ed10976eeb
Fvazquez <foapgenerico@365.stucom.com>
2025-06-17 19:51:31 +0200

    stucom

--- index.js
+++ index.js
@@ -13,10 +13,16 @@ app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
 
 // /api se tiene que mostrar en la pantalla del navegador el contenido del json
 app.get('/api', (req, res) => {
-    // console.log(req.query); 
-    if (req.query.any && req.query.any == "desc") {
+    // console.log(req.query);
+    console.table(req.query); 
+    
+    if (req.query.any && req.query.any == "desc" && req.query.pais == "asc") {
+        let anyo = jsonData.sort((a, b) => b.anyo - a.anyo);
+        return res.json(anyo.sort((a, b) => a.pais - b.pais));
+    } else if (req.query.any && req.query.any == "desc") {
         return res.json(jsonData.sort((a, b) => b.anyo - a.anyo));
-    }
+    }    
+
     res.json(jsonData);
 });
 
