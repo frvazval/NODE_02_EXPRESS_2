0d89ac2811aaab75446933abaa4dc028e8951e58
Fvazquez <foapgenerico@365.stucom.com>
2025-06-17 19:23:17 +0200

    stucom

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ const app = express();
 process.loadEnvFile();
 const PORT = process.env.PORT || 8888;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const jsonData  = require('./ventas.json');
+// console.log(jsonData);
+
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+
+// /api se tiene que mostrar en la pantalla del navegador el contenido del json
+app.get('/api', (req, res) => {
+    console.log(req.query);    
+    res.json(jsonData);
+});
+
 app.listen(PORT, () => console.log(`Example app listening on http://localhost:${PORT}`));
